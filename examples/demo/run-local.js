13a6509a7b5362558114bfde6acef1e2d01587d4
ricky <rickycontact9@gmail.com>
2022-10-18 08:40:30 -0500

    add run-local.js

---
+++ examples/demo/run-local.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const express = require("express")
+const portfinder = require("portfinder")
+const path = require("path")
+const fsPromise = require("fs/promises")
+
+const distDir = path.resolve(__dirname, "../../dist")
+
+async function main() {
+  const app = express()
+
+  app.use("/", express.static(distDir))
+
+  var index = await fsPromise.readFile(
+    path.resolve(__dirname, "./index.html"),
+    {
+      encoding: "ascii",
+    }
+  )
+  index = index.replace("https://cdn.jsdelivr.net/npm/@ricky0123/vad/dist", "")
+  app.get("/index.html", (req, res) => {
+    res.set("Content-Type", "text/html")
+    res.send(Buffer.from(index))
+  })
+
+  const port = await portfinder.getPortPromise()
+
+  const server = await new Promise((resolve, reject) => {
+    const _server = app.listen(port, () => {
+      resolve(_server)
+    })
+  })
+  const url = `http://localhost:${port}/index.html`
+  console.log(`Listening on ${url}`)
+  return { app, port, server, url }
+}
+
+main()
