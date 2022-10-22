c3c01f3a6ad9dacec694102b0e0edd5d9628c0fd
ricky <rickycontact9@gmail.com>
2022-10-22 11:31:54 -0500

    step towards bundling example

---
+++ examples/file-upload/src/index.js
@@ -0,0 +1,39 @@
+import * as vad from "@ricky0123/vad/dist/vad.index"
+
+async function main() {
+  // Feed audio to vad through websocket
+  const ctx = new AudioContext()
+
+  const gainNode = new GainNode(ctx)
+
+  const myvad = await vad.AudioNodeVAD.new(ctx, {
+    onFrameProcessed: (probs) => {
+      const element = document.getElementById("frameCounter")
+      const val = parseInt(element.textContent)
+      element.textContent = val + 1
+    },
+    onSpeechStart: () => {
+      const element = document.getElementById("speechStartCounter")
+      const val = parseInt(element.textContent)
+      element.textContent = val + 1
+    },
+    onSpeechEnd: (arr) => {
+      const element = document.getElementById("speechEndCounter")
+      const val = parseInt(element.textContent)
+      element.textContent = val + 1
+    },
+  })
+  myvad.receive(gainNode)
+  myvad.start()
+
+  window.submitFile = (ev) => {
+    ev.preventDefault()
+    const audioForm = document.getElementById("file-upload").files[0]
+    const audioDataUrl = URL.createObjectURL(audioForm)
+    const audio = Audio(audioDataUrl)
+    const audioNode = ctx.createMediaElementSource(audio)
+    audioNode.connect(gainNode)
+    audio.play()
+  }
+}
+main()
--- examples/file-upload/webpack.config.js
+++ examples/file-upload/webpack.config.js
@@ -1,14 +1,16 @@
 const CopyPlugin = require("copy-webpack-plugin")
-const path = require("path")
 
 module.exports = {
   mode: "development",
-  entry: {},
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "index.html" },
-        { from: "node_modules/vad/dist/*", to: "[name][ext]" },
+        { from: "src/index.html" },
+        {
+          from: "node_modules/@ricky0123/vad/dist/*.worklet.js",
+          to: "[name][ext]",
+        },
+        { from: "node_modules/onnxruntime-web/dist/*.wasm", to: "[name][ext]" },
       ],
     }),
   ],
