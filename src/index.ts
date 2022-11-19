1684edaabb13928f6e05eff148d61bf2df81d9bd
ricky <rickycontact9@gmail.com>
2022-11-18 22:02:57 -0600

    add some comments

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,59 @@ declare var __webpack_public_path__: string
 
 const RECOMMENDED_FRAME_SAMPLES = [512, 1024, 1536]
 
-interface VadOptions {
+/**
+ * Options to customize the behavior of the VAD.
+ */
+export interface VadOptions {
+  /** Callback to run when speech start is detected */
   onSpeechStart: () => any
+
+  /**
+   * Callback to run when speech end is detected.
+   * Takes as arg a Float32Array of audio samples between -1 and 1, sample rate 16000.
+   * This will not run if the audio segment is smaller than `minSpeechFrames`.
+   */
   onSpeechEnd: (audio: Float32Array) => any
+
+  /** Callback to run after each frame. The size (number of samples) of a frame is given by `frameSamples`. */
   onFrameProcessed: (probabilities: SpeechProbabilities) => any
+
+  /** Callback to run if speech start was detected but `onSpeechEnd` will not be run because the
+   * audio segment is smaller than `minSpeechFrames`.
+   */
   signalMisfire: () => any
+
+  /** Threshold over which values returned by the Silero VAD model will be considered as positively indicating speech.
+   * The Silero VAD model is run on each frame. This number should be between 0 and 1.
+   */
   positiveSpeechThreshold: number
+
+  /** Threshold under which values returned by the Silero VAD model will be considered as indicating an absence of speech.
+   * Note that the creators of the Silero VAD have historically set this number at 0.15 less than `positiveSpeechThreshold`.
+   */
   negativeSpeechThreshold: number
+
+  /** After a VAD value under the `negativeSpeechThreshold` is observed, the algorithm will wait `redemptionFrames` frames
+   * before running `onSpeechEnd`. If the model returns a value over `positiveSpeechThreshold` during this grace period, then
+   * the algorithm will consider the previously-detected "speech end" as having been a false negative.
+   */
   redemptionFrames: number
+
+  /** Number of audio samples (under a sample rate of 16000) to comprise one "frame" to feed to the Silero VAD model.
+   * The `frame` serves as a unit of measurement of lengths of audio segments and many other parameters are defined in terms of
+   * frames. The authors of the Silero VAD model offer the following warning:
+   * > WARNING! Silero VAD models were trained using 512, 1024, 1536 samples for 16000 sample rate and 256, 512, 768 samples for 8000 sample rate.
+   * > Values other than these may affect model perfomance!!
+   * In this context, audio fed to the VAD model always has sample rate 16000. It is probably a good idea to leave this at 1536.
+   */
   frameSamples: number
+
+  /** Number of frames to prepend to the audio segment that will be passed to `onSpeechEnd`. */
   preSpeechPadFrames: number
+
+  /** If an audio segment is detected as a speech segment according to initial algorithm but it has fewer than `minSpeechFrames`,
+   * it will be discarded and `signalMisfire` will be run instead of `onSpeechEnd`.
+   */
   minSpeechFrames: number
 }
 
@@ -35,7 +78,7 @@ export function minFramesForTargetMS(
   return Math.ceil((targetDuration * sr) / 1000 / frameSamples)
 }
 
-const defaultVadOptions: VadOptions = {
+export const defaultVadOptions: VadOptions = {
   onSpeechStart: () => {
     log.debug("Detected speech start")
   },
