8a473d87986ab2d5ceebb7482af1d7e74a499b91
ricky <rickycontact9@gmail.com>
2023-03-01 10:39:52 +0800

    wip

--- site/src/js/demo.tsx
+++ site/src/js/demo.tsx
@@ -1,7 +1,7 @@
-import type { RealTimeVADOptions } from "@ricky0123/vad-web"
-import { defaultRealTimeVADOptions, MicVAD } from "@ricky0123/vad-web"
-import React, { useEffect, useState } from "react"
+import { useVAD } from "@ricky0123/vad-react"
+import React, { useState } from "react"
 import { createRoot } from "react-dom/client"
+import { motion } from "framer-motion"
 
 const domContainer = document.querySelector("#demo") as Element
 const root = createRoot(domContainer)
@@ -48,10 +48,9 @@ function ActiveDemo() {
 
   return (
     <div>
-      {vad.listening && "going"}
-      {!vad.listening && "paused"}
-      {vad.userSpeaking && "speaking"}
-      {!vad.userSpeaking && "not speaking"}
+      {vad.listening && vad.userSpeaking && <HighEnergyCube />}
+      {vad.listening && !vad.userSpeaking && <LowEnergyCube />}
+      {!vad.listening && <DeactivatedCube />}
     </div>
   )
 }
@@ -64,119 +63,20 @@ function Errored() {
   return <div>Errored</div>
 }
 
-/*
-###############################################################################
-                          @ricky0123/vad-react
-###############################################################################
-*/
-
-interface ReactOptions {
-  startOnLoad: boolean
+const DeactivatedCube = () => {
+  return (
+    <div className="bg-gradient-to-l from-[#2A2A2A] to-[#474747] h-10 w-10 rounded-[6px]" />
+  )
 }
 
-export interface ReactRealTimeVADOptions
-  extends RealTimeVADOptions,
-    ReactOptions {}
-
-const defaultReactOptions = {
-  startOnLoad: true,
+const LowEnergyCube = () => {
+  return (
+    <motion.div className="bg-gradient-to-l from-[#7928CA] to-[#008080] h-10 w-10 rounded-[6px] low-energy-spin" />
+  )
 }
 
-export const defaultReactRealTimeVADOptions = {
-  ...defaultRealTimeVADOptions,
-  ...defaultReactOptions,
+const HighEnergyCube = () => {
+  return (
+    <motion.div className="bg-gradient-to-l from-[#7928CA] to-[#FF0080] h-10 w-10 rounded-[6px] high-energy-spin" />
+  )
 }
-
-const reactOptionKeys = Object.keys(defaultReactOptions)
-const vadOptionKeys = Object.keys(defaultRealTimeVADOptions)
-
-const _filter = (keys: string[], obj: any) => {
-  return keys.reduce((acc, key) => {
-    acc[key] = obj[key]
-    return acc
-  }, {})
-}
-
-function useOptions(
-  options: Partial<ReactRealTimeVADOptions>
-): [ReactOptions, RealTimeVADOptions] {
-  options = { ...defaultReactRealTimeVADOptions, ...options }
-  const reactOptions = _filter(reactOptionKeys, options) as ReactOptions
-  const vadOptions = _filter(vadOptionKeys, options) as RealTimeVADOptions
-  return [reactOptions, vadOptions]
-}
-
-export function useVAD(options: Partial<ReactRealTimeVADOptions>) {
-  const [reactOptions, vadOptions] = useOptions(options)
-  const [userSpeaking, setUserSpeaking] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [errored, setErrored] = useState<false | { message: string }>(false)
-  const [listening, setListening] = useState(false)
-  const [vad, setVAD] = useState<MicVAD | null>(null)
-  useEffect(() => {
-    ;(async () => {
-      const userOnSpeechStart = vadOptions.onSpeechStart
-      vadOptions.onSpeechStart = () => {
-        setUserSpeaking(true)
-        userOnSpeechStart()
-      }
-      const userOnSpeechEnd = vadOptions.onSpeechEnd
-      vadOptions.onSpeechEnd = (audio) => {
-        setUserSpeaking(false)
-        userOnSpeechEnd(audio)
-      }
-      const userOnVADMisfire = vadOptions.onVADMisfire
-      vadOptions.onVADMisfire = () => {
-        setUserSpeaking(false)
-        userOnVADMisfire()
-      }
-
-      let myvad: MicVAD | null
-      try {
-        myvad = await MicVAD.new(vadOptions)
-      } catch (e) {
-        setLoading(false)
-        if (e instanceof Error) {
-          setErrored({ message: e.message })
-        } else {
-          setErrored({ message: e })
-        }
-        return
-      }
-      setVAD(myvad)
-      setLoading(false)
-      if (reactOptions.startOnLoad) {
-        myvad?.start()
-        setListening(true)
-      }
-    })()
-    return function cleanUp() {
-      if (!loading && !errored) {
-        vad?.pause()
-        setListening(false)
-      }
-    }
-  }, [])
-  const pause = () => {
-    if (!loading && !errored) {
-      vad?.pause()
-      setListening(false)
-    }
-  }
-  const start = () => {
-    if (!loading && !errored) {
-      vad?.start()
-      setListening(true)
-    }
-  }
-  return {
-    listening,
-    errored,
-    loading,
-    userSpeaking,
-    pause,
-    start,
-  }
-}
-
-export { utils } from "@ricky0123/vad-web"
