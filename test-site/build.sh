6c8b34bb1f1387583a0f049f3a93b6a8ec878cf5
ricky <rickycontact9@gmail.com>
2023-03-24 15:13:29 +0800

    test site no longer needs to run vad-react in module

--- test-site/build.sh
+++ test-site/build.sh
@@ -1,6 +1,6 @@
 rm -rf test-site/dist
 mkdir test-site/dist
-npx esbuild ./test-site/src/index.jsx --bundle --sourcemap --format=esm --outfile=./test-site/dist/out.js
+npx esbuild ./test-site/src/index.jsx --bundle --sourcemap --outfile=./test-site/dist/out.js
 cp test-site/index.html test-site/dist
 cp \
     node_modules/@ricky0123/vad-web/dist/*.onnx \
