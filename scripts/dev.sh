1edb8eeda921f7ce15aba9967372e2fd2e0e1fe8
ricky <rickycontact9@gmail.com>
2023-12-02 11:37:25 -0600

    add npm run "dev" script for hot-reloading test-site

---
+++ scripts/dev.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+npm run build-test-site
+
+npx nodemon \
+    --exec "npm run build && npm run build-test-site" \
+    -e js,ts,jsx,tsx,html,css \
+    --watch packages/_common \
+    --watch packages/web/src \
+    --watch packages/react/src \
+    --watch test-site/src \
+    &
+BUILD_PID=$!
+echo "nodemon pid $BUILD_PID"
+
+(
+    cd test-site/dist
+    npx live-server --host=localhost --port=8080 --no-css-inject --wait=500
+) &
+SERVER_PID=$!
+echo "live server pid $SERVER_PID"
+
+trap "kill -INT $BUILD_PID $SERVER_PID" INT
+
+wait
