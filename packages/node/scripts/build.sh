a45adfd49628f50d39e15eb51432d72faff42d6f
Prince Dorcis <princedorcis@gmail.com>
2024-03-24 21:44:51 +0000

    add create dist folder in workspaces

--- packages/node/scripts/build.sh
+++ packages/node/scripts/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf dist
+mkdir dist
 npx tsc
 cp ../../silero_vad.onnx dist
--- packages/web/scripts/build.sh
+++ packages/web/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf dist
+mkdir dist
 npx tsc
 cp ../../silero_vad.onnx dist
 npx webpack -c webpack.config.worklet.js
