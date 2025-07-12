445e8792359ec3d93108f42b8a8ea3b093a40f47
Keyur Maru <kd2maru@gmail.com>
2025-07-12 04:50:00 -0700

    update changelog and format code
    Signed-off-by: Keyur Maru <kd2maru@gmail.com>

--- packages/web/src/resampler.ts
+++ packages/web/src/resampler.ts
@@ -55,7 +55,8 @@ export class Resampler {
   }
 
   private lowPassFilter(sample: number): number {
-    this.lastFilteredValue = this.lastFilteredValue + 
+    this.lastFilteredValue =
+      this.lastFilteredValue +
       this.FILTER_COEFFICIENT * (sample - this.lastFilteredValue)
     return this.lastFilteredValue
   }
@@ -64,34 +65,48 @@ export class Resampler {
     const outputFrame = new Float32Array(this.options.targetFrameSize)
     const ratio = this.options.nativeSampleRate / this.options.targetSampleRate
 
-    if (ratio < 1) { // Upsampling case
-      const inputToOutputRatio = this.options.targetSampleRate / this.options.nativeSampleRate
+    if (ratio < 1) {
+      // Upsampling case
+      const inputToOutputRatio =
+        this.options.targetSampleRate / this.options.nativeSampleRate
 
-      for (let outputIndex = 0; outputIndex < this.options.targetFrameSize; outputIndex++) {
+      for (
+        let outputIndex = 0;
+        outputIndex < this.options.targetFrameSize;
+        outputIndex++
+      ) {
         const inputPosition = outputIndex / inputToOutputRatio
         const inputIndex = Math.floor(inputPosition)
         const fraction = inputPosition - inputIndex
 
         if (inputIndex + 1 < this.inputBuffer.length) {
           const sample1 = this.lowPassFilter(this.inputBuffer[inputIndex] || 0)
-          const sample2 = this.lowPassFilter(this.inputBuffer[inputIndex + 1] || 0)
+          const sample2 = this.lowPassFilter(
+            this.inputBuffer[inputIndex + 1] || 0
+          )
           outputFrame[outputIndex] = sample1 + fraction * (sample2 - sample1)
         } else {
-          outputFrame[outputIndex] = this.lowPassFilter(this.inputBuffer[inputIndex] || 0)
+          outputFrame[outputIndex] = this.lowPassFilter(
+            this.inputBuffer[inputIndex] || 0
+          )
         }
       }
 
-      const samplesUsed = Math.ceil(this.options.targetFrameSize / inputToOutputRatio)
+      const samplesUsed = Math.ceil(
+        this.options.targetFrameSize / inputToOutputRatio
+      )
       this.inputBuffer = this.inputBuffer.slice(samplesUsed)
-    } else { // If ratio >= 1, we're downsampling
+    } else {
+      // If ratio >= 1, we're downsampling
       let outputIndex = 0
       let inputIndex = 0
-  
+
       while (outputIndex < this.options.targetFrameSize) {
         let sum = 0
         let num = 0
         while (
-          inputIndex < Math.min(
+          inputIndex <
+          Math.min(
             this.inputBuffer.length,
             ((outputIndex + 1) * this.options.nativeSampleRate) /
               this.options.targetSampleRate
@@ -107,10 +122,10 @@ export class Resampler {
         outputFrame[outputIndex] = sum / num
         outputIndex++
       }
-  
+
       this.inputBuffer = this.inputBuffer.slice(inputIndex)
     }
-  
+
     return outputFrame
   }
 }
