99b1f87be9074f1a4b3906eb05b4d8f3b7a3cb59
Siddhartha Dutta <siddharthadutta@protonmail.com>
2025-07-26 05:37:33 +0100

    Fix: Pausing Microphone Stream (#198)
    * Fix: Pausing Microphone Stream
    
    * CR: Make Resume Synchronous

--- packages/web/src/real-time-vad.ts
+++ packages/web/src/real-time-vad.ts
@@ -177,13 +177,43 @@ export class MicVAD {
   ) {}
 
   pause = () => {
+    this.stream.getTracks().forEach((track) => {
+      track.stop()
+    })
     this.audioNodeVAD.pause()
     this.listening = false
   }
 
+  resume = async () => {
+    const additionalAudioConstraints =
+      "additionalAudioConstraints" in this.options
+        ? this.options.additionalAudioConstraints
+        : {}
+    this.stream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        ...additionalAudioConstraints,
+        channelCount: 1,
+        echoCancellation: true,
+        autoGainControl: true,
+        noiseSuppression: true,
+      },
+    })
+    this.sourceNode = new MediaStreamAudioSourceNode(this.audioContext, {
+      mediaStream: this.stream,
+    })
+    this.audioNodeVAD.receive(this.sourceNode)
+  }
+
   start = () => {
-    this.audioNodeVAD.start()
-    this.listening = true
+    if (!this.stream.active) {
+      this.resume().then(() => {
+        this.audioNodeVAD.start()
+        this.listening = true
+      })
+    } else {
+      this.audioNodeVAD.start()
+      this.listening = true
+    }
   }
 
   destroy = () => {
